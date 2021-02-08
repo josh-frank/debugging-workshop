36f16f826ce23d804841a593cef3eedc0729c47a
Josh Frank <eurosewusa@gmail.com>
2021-02-08 10:31:09 -0500

    moved username to event listener, created callback renderJoke to fix scope problems

--- index.js
+++ index.js
@@ -2,26 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
   const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
 
-  function fetchJoke(){
+  function fetchJoke( username ){
     fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => renderJoke( username, jokeData.joke ) )
+  }
+
+  function renderJoke( username, jokeText ) {
+    joke = jokeText;
+    newJokeLi.innerHTML = `<span class="username">${username} says:</span> ${joke}`
+    jokeList.appendChild(newJokeLi)
   }
 
   form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    event.preventDefault();
+    const username = document.getElementById('name-input').value
+    if ( username === "" ) return;
+    fetchJoke(username)
   })
 })
